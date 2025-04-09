dc833402d3b376a3ce03a6c6818eb2b7d407fa7e
ufbhatti <111967907+ufbhatti@users.noreply.github.com>
2025-04-09 19:31:54 +0500

    Add files via upload

--- pythoncode.py
+++ pythoncode.py
@@ -6,12 +6,17 @@ Created on Tue Apr  8 19:27:56 2025
 """
 
 from flask import Flask, render_template, request
-app=Flask(__name__)
 
-@app.route('/', method=['GET,POST'])
+app=Flask(__name__)
+@app.route('/form', methods=['GET,POST'])
 def index():
     if request.method=='POST':
         fname=request.form['fname']
         lname=request.form['lname']
         email=request.form['email']
         address=request.form['address']
+        return render_template('result.html',fname=fname,lname=lname,email=email,address=address)
+    return render_template('form.html')
+if __name__=='main':
+    app.reun(debug=True)
+    
